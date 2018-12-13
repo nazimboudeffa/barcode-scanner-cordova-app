82a4c9d671a1d685fc1a42a304d32d98fef6073b
Nazim Boudeffa <boudeffa@gmail.com>
2018-12-13 01:32:52 +0100

    Fixed display results and url

--- www/js/scanner.js
+++ www/js/scanner.js
@@ -1,3 +1,12 @@
+function isURL(string){
+  //https://stackoverflow.com/a/5717133/2107253
+  var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  if (res == null)
+    return false;
+  else
+    return true;
+};
+
 function openURL(url){
-    window.open(url, '_blank', 'location=yes');
-}
+    window.open(url);
+};
